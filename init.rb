d3e61a8cfa8b30ca85ed290b0fb6b8ee4219e84e
Keitan <keitan_lend@hotmail.com>
2016-06-27 16:36:07 -0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -12,11 +12,13 @@ Redmine::Plugin.register :projects_tree_view do
   name 'Projects Tree View plugin'
   author 'Chris Peterson and Github community'
   description 'This is a Redmine plugin which will turn the projects page into a tree view'
-  url 'https://github.com/cforce/projects_tree_view'
+  url 'https://github.com/ichizok/projects_tree_view'
   version '0.0.9'
   requires_redmine :version_or_higher => '3.0.0'
 
   settings :default => {
-    'show_project_progress' => true
+	  'show_project_description' => true ,
+    'show_project_progress' => true ,
+	  'show_project_date' => true ,	
   }, :partial => 'settings/project_tree_settings'
 end
